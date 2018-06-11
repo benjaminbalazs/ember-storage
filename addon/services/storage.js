77561534bd530be5dbfbbbea187302dd370282b3
Benjamin Balazs <benjaminbalazs@gmail.com>
2018-06-11 21:06:05 +0200

    fastboot

--- addon/services/storage.js
+++ addon/services/storage.js
@@ -1,66 +1,101 @@
 import Ember from 'ember';
+import Service, { inject as service } from '@ember/service';
+import EmberObject from '@ember/object';
 
-var storage = {
-  'local': window.localStorage,
-  'session': window.sessionStorage,
-};
+var storage = {};
 
 export default Ember.Service.extend({
-  prefix: 'es',
-  type: 'local',
 
-  _prefix(key) {
-    return this.get('prefix')+'__'+key;
-  },
-  setUpEventListener: Ember.on('init', function() {
-    let self = this,
+    fastboot: service(),
+
+    prefix: 'es',
+    type: 'local',
+
+    _prefix(key) {
+        return this.get('prefix')+'__'+key;
+    },
+
+    setUpEventListener: Ember.on('init', function() {
+
+        let self = this,
         regexp = new RegExp('^('+this.get('prefix')+'__)');
-    this._notify = function(evnt) {
-      self.notifyPropertyChange(evnt.key.replace(regexp, ''));
-    };
-    window.addEventListener('storage', this._notify, false);
-  }),
-  willDestroy() {
-    this._super();
-    window.removeEventListener('storage', this._notify, false);
-  },
-  unknownProperty(k) {
-    var key = this._prefix(k),
-        type = this.get('type');
-    // if we don't use JSON.parse here then observing a boolean doesn't work
-    return storage[type][key] && JSON.parse(storage[type][key]);
-  },
-  setUnknownProperty(k, value) {
-    let key = this._prefix(k),
+
+        this._notify = function(evnt) {
+            self.notifyPropertyChange(evnt.key.replace(regexp, ''));
+        };
+
+        if ( this.get('fastboot.isFastBoot') !== true ) {
+
+            storage = {
+              'local': window.localStorage,
+              'session': window.sessionStorage,
+            };
+
+            window.addEventListener('storage', this._notify, false);
+
+        } else {
+
+        }
+
+    }),
+
+    willDestroy() {
+
+        this._super();
+
+        if ( this.get('fastboot.isFastBoot') !== true ) {
+            window.removeEventListener('storage', this._notify, false);
+        }
+
+    },
+
+    unknownProperty(k) {
+
+        var key = this._prefix(k),
         type = this.get('type');
 
-    if(Ember.isNone(value)) {
-      delete storage[type][key];
-    } else {
-      storage[type][key] = JSON.stringify(value);
-    }
-    this.notifyPropertyChange(k);
-    return value;
-  },
-  clear(keyPrefix) {
-    this.beginPropertyChanges();
-    let prefix = keyPrefix || this.get('prefix'),
+        return storage[type][key] && JSON.parse(storage[type][key]);
+
+    },
+
+    setUnknownProperty(k, value) {
+
+        let key = this._prefix(k),
+        type = this.get('type');
+
+        if ( Ember.isNone(value) ) {
+            delete storage[type][key];
+        } else {
+            storage[type][key] = JSON.stringify(value);
+        }
+        this.notifyPropertyChange(k);
+        return value;
+
+    },
+
+    clear(keyPrefix) {
+
+        this.beginPropertyChanges();
+        let prefix = keyPrefix || this.get('prefix'),
         regexp = new RegExp('^('+prefix+'__)'),
         type = this.get('type'),
         toDelete = [];
 
-    for (var i=0; i < storage[type].length; i++){
-      let key = storage[type].key(i);
-      // don't nuke *everything* in localStorage... just keys that match our pattern
-      if (key.match(regexp)) {
-        toDelete.push(key);
-      }
+        for (var i=0; i < storage[type].length; i++){
+            let key = storage[type].key(i);
+            if (key.match(regexp)) {
+                toDelete.push(key);
+            }
+        }
+
+        toDelete.forEach(function(key) {
+            delete storage[type][key];
+            key = key.replace(regexp, '');
+            this.set(key);
+        }, this);
+
+        this.endPropertyChanges();
+
     }
-    toDelete.forEach(function(key) {
-      delete storage[type][key];
-      key = key.replace(regexp, '');
-      this.set(key);
-    }, this);
-    this.endPropertyChanges();
-  }
+
 });
